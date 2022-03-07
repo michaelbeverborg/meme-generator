2d991b32234b3406933ba7829882ad0396b5c863
Michael <michaelbeverborg@hotmail.com>
2022-03-07 13:19:59 +0100

    Start up of the project

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import trollface from "../images/trollface.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={trollface} alt="Troll face" />
+    </header>
+  );
+};
+
+export default Header;
