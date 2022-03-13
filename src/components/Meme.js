f97ef81ef614d8dbdba1d084b3ac221ad7a8e4f1
Michael <michaelbeverborg@hotmail.com>
2022-03-13 18:16:56 +0100

    Created random image picker

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,21 +1,44 @@
+import memesData from "../memesData";
+import { useState } from "react";
+
 const Meme = () => {
+  const [meme, setMeme] = useState({
+    firstSentence: "",
+    secondSentence: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNum = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNum].url;
+    setMeme(prevMeme => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
   return (
-    <form action="">
-      <input
-        type="text"
-        name="firstSentence"
-        id="firstSentence"
-        placeholder="First Sentence"
-      />
-      <input
-        autocomplete="off"
-        type="text"
-        name="secondSentence"
-        id="secondSentence"
-        placeholder="Second Sentence"
-      />
-      <input type="button" value="Generate" />
-    </form>
+    <main>
+      <form action="">
+        <input
+          type="text"
+          name="firstSentence"
+          id="firstSentence"
+          placeholder="First Sentence"
+        />
+        <input
+          type="text"
+          name="secondSentence"
+          id="secondSentence  "
+          placeholder="Second Sentence"
+        />
+        <input type="button" value="Generate" onClick={getMemeImage} />
+      </form>
+      <img src={meme.randomImage} alt="Meme img" />
+    </main>
   );
 };
 
